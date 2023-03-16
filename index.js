52f66ab44f52e8aea85b7f6a907b200d1eb0ea4d
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-03-16 21:17:18 +0300

    Quick fix

--- index.js
+++ index.js
@@ -2,23 +2,24 @@
 // Write your code here
 
 
-const num1 = 31;
-const num2 = 2;
-const num3 = 10;
-const num4 = 6;
+const num1 = 2;
+const num2 = 31;
+const num3 = 6;
+const num4 = 10;
 
 
 const multiply = num1 * num2;
 
 
-const random = Math.floor(Math.random() * 100) + 1;
+const random = Math.floor(Math.random() * 10) + 1;
 
 
-const mod = num3 % num4;
+const mod = num4 % num3;
 
 
-const max = Math.max(5, 10, 15, 20);
+const max = Math.max(2, 4, 16, 20);
 
 console.log(multiply); 
 console.log(random); 
+console.log(mod);
 console.log(max); 
